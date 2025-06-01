0a40f09084433a5781bd134a3ee4f41db5c91cc1
eduardogomesf <eduardogoomesf@gmail.com>
2025-05-31 23:04:27 -0300

    feat: move routers build strategy to load_routes func

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import os
 import uvicorn
-from fastapi import APIRouter
-from src.create_app import create_app
-from src.controllers.health import router as health_router
+from src.create_app import create_app, load_routes
+from src.controllers import get_routers
 
 if __name__ == "__main__":
   app = create_app("invoice-management-api")
   
-  router = APIRouter(prefix="/api/v1")
-  router.include_router(health_router)
-  
-  app.include_router(router)
-    
+  load_routes(app, get_routers())
+      
   port = int(os.getenv("PORT", 8000))
   uvicorn.run(app, host="0.0.0.0", port=port)
--- src/create_app.py
+++ src/create_app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 def create_app(title = "unnamed-app"):
   app = FastAPI(title=title)
 
   return app
   
+def load_routes(app: FastAPI, routers: list[APIRouter] = []):
+    router = APIRouter(prefix="/api/v1")
+  
+    for r in routers:
+      router.include_router(r)
+      
+    app.include_router(router)
